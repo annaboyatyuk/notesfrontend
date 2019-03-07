36ead52fbdf9a073e20dabc1cff1b4d82c602814
annaboyatyuk <annaboyatyuk@gmail.com>
2019-03-06 22:53:09 -0800

    flexbox notes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,11 +9,11 @@ export default class Footer extends Component {
 
           <div className='green-box'></div>
           
-          {/* <ul className='footer-links'>
-            <li><a href='https://github.com/annaboyatyuk'>github</a></li>
+          <ul className='footer-links'>
+            {/* <li><a href='https://github.com/annaboyatyuk'>github</a></li>
             <li><a href='https://www.linkedin.com/in/annaboyatyuk/'>linkedin</a></li>
-            <li><a href='https://docs.google.com/document/d/12zJmUtfLgMKaffEZGh0zUUcQ84cy6Wmgz95aPjgN_Jo/edit?usp=sharing'>resume</a></li>
-          </ul> */}
+            <li><a href='https://docs.google.com/document/d/12zJmUtfLgMKaffEZGh0zUUcQ84cy6Wmgz95aPjgN_Jo/edit?usp=sharing'>resume</a></li> */}
+          </ul>
 
         </div>
     );
